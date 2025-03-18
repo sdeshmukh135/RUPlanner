26e9166fb6c9b53a002fa1bf7df4e1270bb57945
sanikadeshmukh3 <sanika.deshmukh135@gmail.com>
2025-03-18 11:31:05 -0400

    flask and mongodb addition

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,55 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from pymongo import MongoClient 
+from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(_name_)
+# using mongodb for database management
+# creating a client through MongoClient (client-server architecture)
 
-# define specific routes for specific pages
+app = Flask(__name__)
+CORS(app) # connecting the frontend to the backend
+
+# creating the client
+client = MongoClient('mongodb://localhost:27017/')
+db = client['authndb']
+users_collection = db['users']
+
+# two versions, whether they login or signup
+@app.route('/login', methods=['POST']) # HTTP request
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = users_collection.find_one({'username':username})
+    if user and check_password_hash(user['password'], password): # if it exists and the password is correct
+        return jsonify({'status': 'success', 'message': 'Login successful'}), 200
+    else:
+        return jsonify({'status': 'error', 'message': 'Invalid username or password'}), 401
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password: # if they didn't enter a username or a password (from the frontend)
+        return jsonify({'status': 'error', 'message': 'Missing username or password'}), 400
+    
+    # check if the user exists
+    existing_user = users_collection.find_one({'username':username})
+    if existing_user:
+        return jsonify({'status': 'error', 'message': 'Existing username or password, try againnnnnn'}), 400
+    
+    # otherwise, store the username and password in the database (hash them first for security purposes)
+    hash_password = generate_password_hash(password)
+    users_collection.insert_one({'username': username, 'password': hash_password})
+
+    return jsonify({'status': 'success', 'message': 'User created successfully'}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
