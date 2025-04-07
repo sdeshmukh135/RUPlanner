f9d268e1a549fbcdcdb83067a25c748522fddbb6
sanikadeshmukh3 <sanika.deshmukh135@gmail.com>
2025-04-07 19:06:55 -0400

    updated mongo cluster

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
+import pymongo
 from pymongo import MongoClient 
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
+from urllib.parse import quote_plus
+from pymongo.server_api import ServerApi
+import certifi
+
 
 # using mongodb for database management
 # creating a client through MongoClient (client-server architecture)
@@ -9,11 +14,28 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 CORS(app) # connecting the frontend to the backend
 
+#creating the mongo client 
+#username = quote_plus('<sanikadeshmukh135>') # encoding the username and password
+#password = quote_plus('<Snehnil16>')
+
+username = "sanikadeshmukh135"
+password = "Snehnil16"
+
+uri = 'mongodb+srv://' + username + ':' + password + '@' + 'cluster1.jo3fsmz.mongodb.net/?retryWrites=true&w=majority'
+#'&appName=Cluster1&tlsAllowInvalidCertificates=true'
+
 # creating the client
-url = "mongodb://localhost:27017/"
-client = MongoClient(url) # with unique url
+#url = "mongodb://localhost:27017/"
+client = MongoClient(uri, tls=True, tlsCAFile=certifi.where())
 db = client['authndb']
 users_collection = db['users']
+
+# checking to see if it connected to MongoDB sucessfully
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 # two versions, whether they login or signup
 @app.route('/login', methods=['POST']) # HTTP request
